109bbccb0d43bace03b3e96cdefbfe9041a4ba8d
Saiful Alam Rakib <msa4rakib@gmail.com>
2026-08-13 15:21:15 +0600

    Update history file to support prefix based search

--- history.sh
+++ history.sh
@@ -6,3 +6,8 @@ HISTFILESIZE=20000
 shopt -s histappend
 shopt -s cmdhist
 shopt -s lithist
+
+# Prefix-based history search
+if [[ -f "$HOME/.bash/inputrc" ]]; then
+    bind -f "$HOME/.bash/inputrc"
+fi
